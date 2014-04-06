07274847db04ea85fca01a71109d06e340322377
Tom Buchok <tom@buchok.com>
2014-04-06 11:17:59 -0400

    ensures that weird names are replaced with underscores

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 if (process.env.npm_config_argv == undefined)
-  throw new Error('npm-config must be run inside of an `npm run` node process');
+  console.error('npm-config must be run inside of an `npm run` node process');
+  
 
 module.exports = function() {
-  var key = Array.prototype.join.call(arguments, '_')
+  var key = Array.prototype
+                  .map.call(arguments, function(arg) { 
+                    return arg.replace(/(?!\w)[\x00-\xC0]/g, '_') 
+                  })
+                  .join('_')
     , config = 'npm_config_' + key
     , packageConfig = 'npm_package_config_' + key
     , value = process.env[config] || process.env[packageConfig] 
